f8def2afc6214d069682b737d7c300513754c633
knuksg <knuksg@naver.com>
2024-05-13 20:20:31 +0900

    test

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -2,8 +2,8 @@ const express = require("express");
 const { addUser, getUsers, getUserById, updateUser, deleteUser } = require("../controllers/userController");
 const router = express.Router();
 
-router.post("", addUser);
-router.get("", getUsers);
+router.post("/", addUser);
+router.get("/", getUsers);
 router.get("/:uid", getUserById);
 router.put("/:uid", updateUser);
 router.delete("/:uid", deleteUser);
