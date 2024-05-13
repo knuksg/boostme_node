40e6301ee5cb4c1bc4f293c4812aa998ef34007c
knuksg <knuksg@naver.com>
2024-05-13 17:20:45 +0900

    test

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ app.use("/api", postRoutes);
 app.use("/api", yogaRoutes);
 app.use("/api", workoutRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
