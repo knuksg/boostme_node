710bdd315d3f3e56ed161c06a7cf343b00873220
knuksg <knuksg@naver.com>
2024-06-11 14:48:46 +0900

    test

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,7 +1,9 @@
 const admin = require("../firebaseAdmin");
 
 const verifyToken = async (req, res, next) => {
+    console.log("req.headers", req.headers);
     const idToken = req.headers.authorization?.split("Bearer ")[1];
+    console.log("idToken", idToken);
 
     if (!idToken) {
         return res.status(401).json({ error: "Unauthorized" });
